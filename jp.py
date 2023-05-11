318ee8902ab7cd73b0ff4fd61b2c7e52f5142504
rayaansyed <132567328+rayaansyed@users.noreply.github.com>
2023-05-11 03:24:26 -0700

    Create jp.py

---
+++ jp.py
@@ -0,0 +1,6 @@
+a=int(input("enter first number"))
+b=int(input("enter second number))
+if a>b:
+   print("a is the biggest number that is", a)
+else:
+   print("b is the greatest number that is", b)
